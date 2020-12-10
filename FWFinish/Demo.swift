2386ece8b274f6d319ec85dd92cbf357871b4d75
annt <annt598@viettel.com.vn>
2020-12-10 15:47:50 +0700

    update code

---
+++ FWFinish/Demo.swift
@@ -0,0 +1,16 @@
+//
+//  Demo.swift
+//  FWFinish
+//
+//  Created by annt on 12/10/20.
+//  Copyright © 2020 annt. All rights reserved.
+//
+
+import Foundation
+public class Demo {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ", message)
+    }
+}
---
+++ FWFinish/FWFinish.h
@@ -0,0 +1,19 @@
+//
+//  FWFinish.h
+//  FWFinish
+//
+//  Created by annt on 12/10/20.
+//  Copyright © 2020 annt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FWFinish.
+FOUNDATION_EXPORT double FWFinishVersionNumber;
+
+//! Project version string for FWFinish.
+FOUNDATION_EXPORT const unsigned char FWFinishVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWFinish/PublicHeader.h>
+
+
